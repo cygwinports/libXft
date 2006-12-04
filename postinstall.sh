fae2f901dd706f53c83b1a9edf94c31ee31e7725
yselkowitz <yselkowitz@e127d73d-b18e-4ae1-a744-a22e6a3ccd30>
2006-12-04 05:37:43 +0000

    X11R7.2
    git-svn-id: https://cygwin-ports.svn.sourceforge.net/svnroot/cygwin-ports/ports/trunk@600 e127d73d-b18e-4ae1-a744-a22e6a3ccd30

--- postinstall.sh
+++ postinstall.sh
@@ -1,1 +1,1 @@
-find /usr/lib -name '*.la' | xargs -r sed -i -e 's:/usr/X11R6/lib/libXft\.la:/usr/lib/libXft\.la:g'
+find /usr/lib -name '*.la' -exec sed -i -e 's#/usr/X11R6/lib/libXft\.la#/usr/lib/libXft\.la#g' '{}' +
